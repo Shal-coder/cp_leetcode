e4959a1404ea22323693ac263cc014289be521e7
Shal-coder <ltinsaebirhanu@gmail.com>
2025-04-10 21:16:23 +0300

    Add Digits

---
+++ AddDigits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num >= 10:
+            sum_of_digits = 0
+            while num > 0:
+                sum_of_digits += num % 10
+                num //= 10
+            num = sum_of_digits
+        return num
+num = 38
+result= Solution().addDigits(num)
+print(result)
+        
